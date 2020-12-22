21ceb00e5bc6850a48285f1aeab816c9d816378b
Daniele <danlup94@gmail.com>
2020-12-22 10:09:41 +0100

    Correzione esercizio

--- index.php
+++ index.php
@@ -22,17 +22,21 @@
 
     <p>
 
-      <?php $test = "Sono un paragrafo e questa è una brutta parola: " ?>
+      <?php $badword = $GET_['badword']; ?>
 
-      <?php $bad = "democristiano" ?>
+      <?php $test = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ";
+            $lng = strlen($test);
+      ?>
 
-      <?php echo $test . $bad ; ?>
+      <?php $sostituto = str_replace($badword, '***' , $test) ; ?>
+
+      <?php echo $test . ' (' . $lng . ')' ; ?>
 
       <br>
 
       <br>
 
-      <?php echo $test . $_GET["censura"]; ?>
+      <?php echo $sostituto; ?>
 
     </p>
 
